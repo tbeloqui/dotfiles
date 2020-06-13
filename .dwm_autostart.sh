2db7ec686b3754533666974f16cf4956e9198e66
Tulio Beloqui <tulio.beloqui@pexip.com>
2020-06-13 12:39:52 -0300

    added xrandr

--- .dwm_autostart.sh
+++ .dwm_autostart.sh
@@ -5,5 +5,8 @@ nm-applet &
 slack &
 firefox &
 feh --bg-scale $HOME/Pictures/wallpaper.png &
-bash $HOME/.dwm_bar.sh &
+bash /home/fr30n/.dwm_bar.sh &
 
+if [ ! -z "$(xrandr | grep "DP1 disconnected")" ]; then
+    xrandr --output eDP1 --mode 1920x1080 --scale 1x1 --dpi 70 --rate 60 &
+fi
