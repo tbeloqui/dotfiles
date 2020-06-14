a488c99f0390db50ebb64c7d2bd0b5951caf66d4
Tulio Beloqui <tulio.beloqui@pexip.com>
2020-06-14 20:40:08 -0300

    updated dwm autostart

--- .dwm_autostart.sh
+++ .dwm_autostart.sh
@@ -2,10 +2,12 @@
 
 parcellite &
 nm-applet &
+xfce4-volumed &
 slack &
 firefox &
 feh --bg-scale $HOME/Pictures/wallpaper.png &
 bash /home/fr30n/.dwm_bar.sh &
+rxvt-unicode -e $HOME/.scripts/tmux.sh &
 
 if [ ! -z "$(xrandr | grep "DP1 disconnected")" ]; then
     xrandr --output eDP1 --mode 1920x1080 --scale 1x1 --dpi 70 --rate 60 &
